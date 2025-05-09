1990b96989b263bdc4c346dd84d12054c4b5adc3
xevasme <106179739+xevasme@users.noreply.github.com>
2025-05-09 16:52:40 -0500

    Se importan los  primeros datos de medicion del fondo y de las muestras, además del código arduino utilizado

---
+++ Estadistica_de_Poisson/Código arduino/sketch_may7b.ino
@@ -0,0 +1,43 @@
+
+int contador = 0;
+int pin = 8;
+
+long t = 0;
+
+long t0 = 0;
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pin, INPUT);
+
+  t0 = 0;
+
+}
+
+void loop() {
+  if (digitalRead(pin)==1){
+    contador++;
+    //Serial.println(contador);
+    //delay(10);
+
+  }
+
+
+
+if (millis()-t0 >= 5228) {
+
+  //t = millis()-t0;
+  // Serial.print(t0);
+  // Serial.print(',');
+  // Serial.print(millis());
+  // Serial.print(',');
+  Serial.println(contador);
+  contador = 0;
+  t0 = millis();
+}
+
+
+
+
+}
